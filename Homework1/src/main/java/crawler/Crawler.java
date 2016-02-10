b4ff4a0a0bfd1336cdd957d0572057dd6bc1b636
genusgant <genusgant@gmail.com>
2016-02-10 15:18:46 -0800

    updates

--- Homework1/src/main/java/crawler/Crawler.java
+++ Homework1/src/main/java/crawler/Crawler.java
@@ -1,6 +1,8 @@
 package crawler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
@@ -15,32 +17,80 @@ import model.Link;
 public class Crawler {
 	
 	private String inputUrl;
+	private int level;
 	private List[] ExtLinks;
 	private Document source;
+	private Queue<Link> UrlsToCrawl = new LinkedList<Link>();
+	private Queue<Link> UrlsCrawled = new LinkedList<Link>();
 	
 	public Crawler(Link InitialUrl, Queue<Link> UrlsToCrawl, Queue<Link> UrlsCrawled)
 	{
+		
 		this.inputUrl = InitialUrl.getLink();
-				
+		this.level = InitialUrl.getLevel();
+		this.UrlsToCrawl = UrlsToCrawl;
+		this.UrlsCrawled = UrlsCrawled;
+		
+		getInnerUrl();
 	}
 	
-	public void PollContinuously()
+	public Crawler(Queue<Link> UrlsToCrawl, Queue<Link> UrlsCrawled)
+	{
+		
+		this.UrlsToCrawl = UrlsToCrawl;
+		this.UrlsCrawled = UrlsCrawled;
+
+	}
+	
+	public void getInnerUrl()
 	{
 		try {
-			Elements hyperLinks;
-			
+			Elements hyperLinks;			
 			
 			source = Jsoup.connect(inputUrl).userAgent("User-Agent").timeout(10000).execute().parse();
-
+            
 			hyperLinks = source.select("a[href]");
 			
+			Link cl = new Link();
+			cl.setLevel(this.level);
+			cl.setLink(inputUrl);
+			UrlsCrawled.add(cl);
+			
 			for (Element link : hyperLinks) {
-				System.out.println("\nlink : " + ((Node)link).attr("abs:href"));
-				System.out.println("text : " + ((org.jsoup.nodes.Element) link).text());	
+//				System.out.println("\nlink : " + ((Node)link).attr("abs:href"));
+//	//			list.add(((Node)link).attr("abs:href"));
+				Link l = new Link();
+				l.setLevel(this.level+1);
+				l.setLink(((Node)link).attr("abs:href"));
+				UrlsToCrawl.add(l);
+//				System.out.println("text : " + ((org.jsoup.nodes.Element) link).text());	
 			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		
+	}
+	
+	public void PollContinuously()
+	{
+		Link toCrawl = new Link();
+		toCrawl = UrlsToCrawl.poll();
+		this.inputUrl = toCrawl.getLink();
+		this.level = toCrawl.getLevel();
+		boolean flag = true;
+		
+		for(Link l : UrlsCrawled) 
+		{
+			if(l.getLink().equals(inputUrl))
+			{
+			flag = false;	
+			}
+		}
+		
+		if (flag)
+		{
+			getInnerUrl();
 		}
 	}
 	
--- Homework1/src/main/java/crawler/CrawlerManager.java
+++ Homework1/src/main/java/crawler/CrawlerManager.java
@@ -10,6 +10,9 @@ public class CrawlerManager {
 	public static Queue<Link> UrlsToCrawl = new LinkedList<Link>(); 
 	public static Queue<Link> UrlsCrawled = new LinkedList<Link>(); 
 	
+	public static int counter = 0;
+	public static int o_count = 0; 
+	public static int n_count = 0;
 	
 
 	public static void main(String[] args) {
@@ -17,17 +20,82 @@ public class CrawlerManager {
 		Link InitialUrl = new Link();		
 		InitialUrl.setLink("http://stackoverflow.com/questions/921262/how-to-download-and-save-a-file-from-internet-using-java");
 		InitialUrl.setLevel(0);
+		int counter = 0;
 		
 		Crawler c1 = new Crawler(InitialUrl, UrlsToCrawl, UrlsCrawled);
 		
-		c1.PollContinuously();
+		for(Link l : UrlsToCrawl)
+		{
+			l.getLink();
+			System.out.println(l.getLevel()+" "+l.getLink());
+			counter++;
+		}
+		System.out.println("total "+counter+" links" );
 		
+		Crawler c2 = new Crawler(UrlsToCrawl, UrlsCrawled);
 		
+		c2.PollContinuously();
+		System.out.println("-------------------------------------------------------------------");
+		counter =0;
+		int count =0; 
+		int count1 = 0;
+		for(Link l : UrlsToCrawl)
+		{
+			l.getLink();
+			System.out.println(l.getLevel()+" "+l.getLink());
+			counter++;
+			if (l.getLevel() == 1)
+			{
+				count++;
+			}
+			else
+			{
+				count1++;
+			}
+		}
+		System.out.println("total "+counter+" new links. previous "+count+" now "+count1 );
+	
 		
+		Crawler c3 = new Crawler(UrlsToCrawl, UrlsCrawled);
+		
+		c3.PollContinuously();
+		count();
+		c2.PollContinuously();
+		count();
 		
 		
 		
 
 	}
+	
+	public static void count()
+	{
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+		int temp;
+		temp =counter;
+		counter =0;
+		o_count=0;
+		n_count=0;
+		
+		
+		for(Link l : UrlsToCrawl)
+		{
+			l.getLink();
+			System.out.println(l.getLevel()+" "+l.getLink());
+			counter++;
+			if (l.getLevel() == 1)
+			{
+				o_count++;
+			}
+			else
+			{
+				n_count++;
+			}
+		}
+		System.out.println("total "+counter+" new links. previous "+o_count+" now "+n_count+" diff "+temp );
+		System.out.println("-------------------------------------------------------------------");
+
+	}
+	
 
 }
